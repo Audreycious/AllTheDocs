448abab9aaa94b4618ea25340e4d3d00cbfec8b0
Audreycious <porcupine4@gmail.com>
2019-09-26 18:05:07 -0500

    init commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      searchQuery: null,
+      stackOverflowData: null,
+      docData: null,
+      youtubeData: null,
+    }
+  }
+
+
+  
+  render() {
+    return (
+      <div className="App">
+        
+      </div>
+    )
+  }
 }
 
 export default App;
