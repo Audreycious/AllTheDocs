f75a7945116318cbf07233aa2f66a607155f322a
Audreycious <porcupine4@gmail.com>
2019-10-08 22:05:27 -0500

    updated config

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-    API_ENDPOINT: `https://pure-cliffs-28827.herokuapp.com/`,
+    API_ENDPOINT:`https://allthedocs.herokuapp.com/`,
     API_KEY: process.env.REACT_APP_API_KEY,
 }
